8422d6058d91b0c176f02d9119da2abf8c948655
Nishthaa Jain <nishthaa2003@gmail.com>
2025-01-11 21:25:11 +0530

    axios

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const instance = axios.create({
     // baseURL:'http://127.0.0.1:4444',
-    baseURL:'http://localhost:4444',
+    // baseURL:'http://localhost:4444',
+    baseURL:'https://kanbanboard-backend.vercel.app',
     withCredentials: true
 });
 
